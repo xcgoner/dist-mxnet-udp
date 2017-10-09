c640abbe40b60e1e8516ecb3dec1e9ed617c3652
cx2 <cx2@illinois.edu>
2017-10-08 21:54:55 -0500

    testing

--- example/image-classification/train_mnist.py
+++ example/image-classification/train_mnist.py
@@ -25,8 +25,8 @@ logging.basicConfig(level=logging.DEBUG)
 os.environ["MXNET_KVSTORE_BIGARRAY_BOUND"] = "2000"
 os.environ["DMLC_NUM_KEYRANGE"] = "20"
 os.environ["PS_VAN"] = "zmq"
-# os.environ["MXNET_MERGE_THRESHOLD"] = "2"
-# os.environ["MXNET_MERGE_TAU_MILLISECOND"] = "20"
+os.environ["MXNET_MERGE_THRESHOLD"] = "4"
+os.environ["MXNET_MERGE_TAU_MILLISECOND"] = "20"
 # os.environ["DMLC_PS_PULL_THRESHOLD"] = "0.7"
 # os.environ["MXNET_KVSTORE_SERVER_USE_HISTORY"] = "1"
 from common import find_mxnet, fit
